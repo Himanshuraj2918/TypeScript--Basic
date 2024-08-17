3ea91a331f5b23bde9aea7b7a698734badc666e3
Himanshu2918 <himanshuraj2918@gmail.com>
2024-08-17 14:44:35 +0530

    literals type

---
+++ dist/literals.js
@@ -0,0 +1,12 @@
+"use strict";
+// Literals
+// 1. String Literals
+// 2. Numerical Literals
+// 3. Boolean Literals
+// ************Number Literals************
+let number;
+number = 12; // valid
+// number = 13; //not valid error
+// ***************String Literals**************
+let stringName;
+// stringName = "Good"  //not valid error
---
+++ literals.ts
@@ -0,0 +1,19 @@
+// Literals
+
+// 1. String Literals
+// 2. Numerical Literals
+// 3. Boolean Literals
+
+
+// ************Number Literals************
+let number:12;
+
+number = 12; // valid
+
+// number = 13; //not valid error
+
+// ***************String Literals**************
+
+let stringName: "Harish";
+
+// stringName = "Good"  //not valid error
