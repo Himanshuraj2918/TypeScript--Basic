2ada3c0bb5e2459b42ba6c193bf76a7f7e0b51e4
Himanshu2918 <himanshuraj2918@gmail.com>
2024-08-17 13:15:34 +0530

    about function

--- dist/method.js
+++ dist/method.js
@@ -3,3 +3,31 @@
 function ab(a, b) {
     console.log("Hey");
 }
+// Named Function && Explicit return type
+function abcd() {
+}
+//Anonymous Function
+const ano = function () {
+};
+//Arrow function
+const abcd1 = () => {
+};
+//Implict return type Function
+function abcd2() {
+    return "hey";
+}
+//optional parameters
+function user(name, age, gender) {
+    //optional prameters it depend upon user it will give or not 
+    // so for making it optional we use  ?
+}
+user("Harey", 22);
+//default parameters
+function user1(name = "Harey") {
+    //If any one not provide parameters it will use default value.
+}
+//Rest parameters 
+function name3(...names) {
+    console.log(names);
+}
+name3("Hare", "Krishn", "Krishn", "Krishn");
--- method.ts
+++ method.ts
@@ -4,3 +4,41 @@ function ab(a: string , b: number): void{
        console.log("Hey");
        
 }
+
+// Named Function && Explicit return type
+function abcd(): void{
+}
+
+//Anonymous Function
+const ano= function(): void{
+}
+
+//Arrow function
+const abcd1 = ():void=>{
+}
+
+//Implict return type Function
+function abcd2(){
+return "hey"
+}
+
+//optional parameters
+function user(name:string, age:number, gender?:string){
+       //optional prameters it depend upon user it will give or not 
+       // so for making it optional we use  ?
+}
+user("Harey",22)
+
+//default parameters
+
+function user1(name:string = "Harey"){
+       //If any one not provide parameters it will use default value.
+}
+
+//Rest parameters 
+
+function name3(...names: string[]){
+    console.log(names);
+    
+}
+name3("Hare","Krishn","Krishn","Krishn")
